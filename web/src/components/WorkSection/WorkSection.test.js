0c48fe02a9c647bd8cd6654a193605f23b6fc891
jlintag <jlintag@gmail.com>
2021-05-14 18:36:17 -0400

    passing tests

--- web/src/components/WorkSection/WorkSection.test.js
+++ web/src/components/WorkSection/WorkSection.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { render, screen } from 'react-dom'
+import { screen, render,logDOM } from '@testing-library/react'
 import WorkSection from './WorkSection'
 
 describe('WorkSection', () => {
   it('renders heading', () => {
     render(<WorkSection/>)
-    const heading = screen.getByText(/Job Title/i)
+    const heading = screen.getByText(/Users/i)
+    expect(heading).toBeInTheDocument();
   })
 })
